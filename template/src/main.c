54db24d934de4fd883d54da2865fd8e79819f671
Moin Ahmed <63891600+MoinA0529@users.noreply.github.com>
2021-01-31 02:39:59 +0530

    Update main.c

--- template/src/main.c
+++ template/src/main.c
@@ -11,14 +11,20 @@ int main(int argc, char *argv[])
     // gtk_builder_add_from_file (builder, "glade/window_main.glade", NULL);
     // Update October 2019: The line below replaces the 2 lines above
     builder = gtk_builder_new_from_file("glade/window_main.glade");
-
+    //gtk_builder_new_from_file(const gchar *filename ): builds GTK UI definition in the given file. 
+    //gtk_builder builds a widget from our glade file.
+    
     window = GTK_WIDGET(gtk_builder_get_object(builder, "window_main"));
     gtk_builder_connect_signals(builder, NULL);
-
+    //gtk_builder(GtkBuilder* builder,gpointer user_data) connects the signals from the user to widget
+    
     g_object_unref(builder);
 
     gtk_widget_show(window);
+    //call this function to make the window visible
+    
     gtk_main();
+    //main: hand control to GTK main loop and process events(received from signals etc.)
 
     return 0;
 }
